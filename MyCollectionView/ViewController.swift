508a828bd47c887e53f56ec8a7467df901b471ab
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-09 16:42:58 +0900

    Initial Commit

---
+++ MyCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCollectionView
+//
+//  Created by 三浦　一真 on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
